513d45e4cf0222c21d368caab06c405743fb52b3
vadog <vadog2672@gmail.com>
2020-07-18 11:04:51 -0700

    added a way to get random pictures

---
+++ randompictest.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+import os
+
+
+def getPic():
+    pictureDirectory = ["recycle", "trash", "yardtrim"]
+    trashTypeNum = random.randrange(0, 3)
+    trashType = os.listdir(pictureDirectory[trashTypeNum])
+    randomPic = trashType[random.randrange(0, len(trashType))]
+    path = os.path.join(pictureDirectory[trashTypeNum], randomPic)
+    return path, trashTypeNum,
+
+
+print(getPic())
